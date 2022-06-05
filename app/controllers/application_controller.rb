840d6f09e2a9187e495e32b6685eb2cadc3195e2
mikewalmeida <michael.almeida@kellogg.northwestern.edu>
2022-06-05 08:17:18 +0000

    image upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
     # puts "------------------ code before every request ------------------"
     @current_user = User.find_by({ "id" => session["user_id"] })
   end
+
+
+  
 end
 
