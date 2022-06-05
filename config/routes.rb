0627c95ad6c3891d6bbcebc2636dbfc8c8a54d65
mikewalmeida <michael.almeida@kellogg.northwestern.edu>
2022-06-05 05:25:48 +0000

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources "posts"
   resources "sessions"
   resources "users"
+  resources "sessions"
+
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
 end
